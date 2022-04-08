06ff41e20d4b7f1a98e749a7f16a2a73e2d735a6
KimJunSik <pkwaw@naver.com>
2022-04-08 15:39:10 +0900

    author 생성

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -1,6 +1,10 @@
 import os.path
 
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
 
 
 # Create your models here
@@ -13,8 +17,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
-        return f'[{self.pk}]  [{self.title}]'
+        return f'[{self.pk}]  [{self.title}] :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
