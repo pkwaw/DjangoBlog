9fbdd760d79ad72fd0340d7e13df06e426910b14
KimJunSik <pkwaw@naver.com>
2022-04-08 16:13:30 +0900

    post에 category필드 추가

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -1,4 +1,5 @@
 import os.path
+from turtle import mode
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -10,6 +11,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 
 # Create your models here
@@ -17,13 +21,15 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    hook_msg = models.TextField(blank=True)
+
     head_image = models.ImageField(upload_to = 'blog/images/%Y/%m/%d/', blank = True)
     attached_file = models.FileField(upload_to = 'blog/files/%Y/%m/%d/', blank = True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     def __str__(self):
         return f'[{self.pk}]  [{self.title}] :: {self.author}'
 
