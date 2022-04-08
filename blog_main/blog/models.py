60806512bc09819e0a2e09735f1912046dd9e166
KimJunSik <pkwaw@naver.com>
2022-04-08 17:22:42 +0900

    카테고리 뷰 생성, 태그 모델 추가

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -4,6 +4,16 @@ from turtle import mode
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -13,6 +23,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}'
 
 
 
@@ -25,11 +38,14 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to = 'blog/images/%Y/%m/%d/', blank = True)
     attached_file = models.FileField(upload_to = 'blog/files/%Y/%m/%d/', blank = True)
+
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
+
     def __str__(self):
         return f'[{self.pk}]  [{self.title}] :: {self.author}'
 
--- blog_main/blog/urls.py
+++ blog_main/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     #path('', views.index),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.show_category_posts)
 ]
